929527922fc0814e742d4fc3e1e093e9a28c9872
sahilshetty10 <65429226+sahilshetty10@users.noreply.github.com>
2022-09-29 13:32:21 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ const index = ({ data }: any) => {
         <BarChart data={data} height={600} width={600}>
           <Bar dataKey="closingPrice">
             {data.map((entry: any, index: any) => (
-              <Cell fill={entry.longShortRatio > 2 ? "#fff" : "#000"} />
+              <Cell fill={entry.longShortRatio > 2 ? "#fff" : "#000"} key={index}/>
             ))}
           </Bar>
         </BarChart>
